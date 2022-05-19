df3598f660b9a84e3d6d13c9ea9e347d40a260ce
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-20 02:10:44 +0530

    added route to flight-info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FlightInfoComponent } from './flight-info/flight-info.component';
 import { AllFlightsComponent } from './all-flights/all-flights.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: LoginPageComponent},
   {path:'all-flights', component: AllFlightsComponent },
+  {path:'flight-info', component: FlightInfoComponent },
 ];
 
 @NgModule({
