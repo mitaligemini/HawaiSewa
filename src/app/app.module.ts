40e40536c78660e0814b3da762cc7a28fee3f688
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-19 15:34:10 +0530

    routes outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightInfoComponent } from './flight-info/flight-info.component';
+import { AllFlightsComponent } from './all-flights/all-flights.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightInfoComponent,
+    AllFlightsComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
