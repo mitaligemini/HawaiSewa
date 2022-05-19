11336dfcbd148c642385673ed2289774dbda0f9b
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-19 15:33:41 +0530

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AllFlightsComponent } from './all-flights/all-flights.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginPageComponent},
+  {path:'all-flights', component: AllFlightsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
