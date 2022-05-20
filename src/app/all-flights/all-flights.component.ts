50883308782ba9058d0fa1d0ad7d49c427b53969
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-20 23:37:35 +0530

    final done

--- src/app/all-flights/all-flights.component.ts
+++ src/app/all-flights/all-flights.component.ts
@@ -22,24 +22,46 @@ export class AllFlightsComponent implements OnInit {
     this.flights = this.allFlights.getFlight();
     console.log(this.flights);
   }
-
+  
+  isDuration:boolean=false;
   duration(): void {
+    this.isDeparture=false;
+    this.isDuration=true;
+    this.isPrice=false;
+    this.isArrival=false;
     this.flights.sort((objA: any, objB: any) =>
     objA.duration > objB.duration ? 1 : -1
     )
     console.log(this.flights);
   }
 
+  isDeparture:boolean=false;
+
   departure(): void {
+    this.isDeparture=true;
+    this.isDuration=false;
+    this.isPrice=false;
+    this.isArrival=false;
     this.flights.sort((objA: any, objB: any) =>
       objA.departure > objB.departure ? 1 : -1
     )
   }
+  isPrice:boolean=false;
   price(): void {
+      this.isPrice=true;
+      this.isDeparture=false;
+      this.isDuration=false;
+      this.isArrival=false;
     this.flights.sort((objA: any, objB: any) =>
       (objA.ticketPrice > objB.ticketPrice ? 1 : -1))
   }
+
+  isArrival:boolean=false;
   arrival(): void {
+    this.isDeparture=false;
+    this.isDuration=false;
+    this.isPrice=false;
+    this.isArrival=true;
     this.flights.sort((objA: any, objB: any) =>
     objA.arrival > objB.arrival ? 1 : -1
     )
